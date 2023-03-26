fbe076540c916edcb1df9bc6ea9bb7a86a25179d
sigma-libra <sabrina.ckall@gmail.com>
2023-03-26 22:20:46 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <LeftBar />
+        <RightBar />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+function LeftBar() {
+  return (<div className="left-section">
+    <h1>Sabrina Kall</h1>
+    <img src={require("./profile_photo.jpg")} height={200} alt="profile_photo" />
+    <h2>Software Engineer</h2>
+    <p>Born 10th October 1996</p>
+    <nav className="links-container">
+      <a href="https://www.linkedin.com/in/sabrina-kall/" target="_blank" rel="noreferrer">linkedin</a>
+      <a href="https://github.com/sigma-libra" target="_blank" rel="noreferrer">github</a>
+      <a href="https://stackoverflow.com/users/8788182/sigma1510" target="_blank" rel="noreferrer">stackoverflow</a>
+    </nav>
+  </div>)
+}
+
+function RightBar() {
+  return (
+    <div className="right-section">
+      <h1>Curriculum</h1>
+      <h1>Hobbies</h1>
+      <h1>Skills</h1>
+      <h1>Passions</h1>
+    </div>
+  )
+}
