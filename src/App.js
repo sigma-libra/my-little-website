3b9d8d7c11070b7103da4912c9d5f4f315084bee
sigma-libra <sabrina.ckall@gmail.com>
2023-03-30 18:38:11 +0200

    add skills

--- src/App.js
+++ src/App.js
@@ -53,10 +53,18 @@ function SkillsBar() {
   return (
     <div className="skills-section">
       <h1>Skills</h1>
-      <KeywordList keywords={["Java", "Kotlin", "Go"]} />
+      <KeywordList keywords={["Architecture Design", "API Design (REST, Async)", "Database Design",
+        "End-to-end Testing", "DevOps", "Product Integration", "Machine Learning"]} />
+      <h1>Tech Stack</h1>
+      <KeywordList keywords={["Kubernetes", "Helm", "Docker", "AWS",
+        "Java (+ Spring + Junit)", "Kotlin", "Go", "C++", "JavaScript (+ HTML + CSS)",
+        "Python (+ Flask + Pandas)", "Jupyter Notebook", "Pytorch", "Spark", "Hadoop",
+        "Kafka", "Flutter", "React", "SQL (+ Postgres)", "MongoDB", "DynamoDB", "Elasticsearch",
+        "Linux", "Git", "CI/CD Pipelines", "JIRA + Confluence", "SCRUM"]} />
       <h1>Hobbies</h1>
       <KeywordList keywords={["Coding", "Reading", "Writing", "Salsa", "Hiking", "Travelling", "Long-distance Running", "Winter Swimming"]} />
       <h1>Passions</h1>
+      <KeywordList keywords={["Clean Code", "Mentoring"]} />
     </div>
   )
 }
